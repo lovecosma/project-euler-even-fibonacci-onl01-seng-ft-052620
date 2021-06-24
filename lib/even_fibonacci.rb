9e1f8c207116051292fc83ae2125a740a45e6e49
lovecosma <williamsmatthew333@gmail.com>
2021-06-23 20:34:20 -0600

    commit tree fe9a4ec93264c4c60cd2fa1ef3a9ec01910c89ec

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,19 @@
+require 'pry'
 # Implement your procedural solution here!
+def even_fibonacci_sum limit
+    fib_value = 2
+    prev_value = 1
+    next_value = 0
+    sum = 0
+    while fib_value < limit
+
+        if(fib_value % 2 == 0)
+            sum += fib_value
+        end
+
+        next_value = prev_value + fib_value 
+        prev_value = fib_value
+        fib_value = next_value
+    end 
+    sum
+end 
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def sum
+        prev_value = 1
+        fib_value = 2
+        next_value = 0
+        sum = 0
+
+        while fib_value < @limit
+
+            if(fib_value % 2 == 0)
+                sum += fib_value
+            end 
+
+            next_value = fib_value + prev_value
+            prev_value = fib_value
+            fib_value = next_value
+
+        end 
+        sum
+    end 
+
+
+end 
